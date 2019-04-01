61de648f635bb50e6abd4fdbf76385337d9fd0f7
tntcvad <48907412+tntcvad@users.noreply.github.com>
2019-04-01 11:53:31 +0700

    câu lệnh switch case

--- switch and case.js
+++ switch and case.js
@@ -1,14 +1,14 @@
 function week_1() {
     /* cấu trúc cú pháp
-    switch ( Biến){
+    switch ( biến){
 
       case  value_1: { câu lệnh 1 ; câu lệnh 2...; break};
       case  value_2: {câu lệnh 2; break;}
       case  value_n: { câu lệnh n;break};
       defaul:         {câu lệnh; break;}
 
-      - biến là được đem ra so sánh với value;
-      - value có thể là giá trị hoacj là chuỗi
+      - biến là được đem ra so sánh == với value;
+      - value có thể là giá trị hoac là chuỗi
       - mỗi case tương ứng giữa so sánh giữa biến và value; ( khi biến = value_1 và khong có câu lệnh break trong biểu
        thức thì sẽ chay cả câu lệnh 2 .. câu n đến khi nào gặp câu lệnh break thì dừng lại và thoát ra
         trong 1 biểu thức có thể có nhiều câu lệnh .
